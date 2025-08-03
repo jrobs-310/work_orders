545f64699cc8e051f8944279ca3a73dbda8ae3b1
Jon Robins <jon@forcemetrics.ai>
2025-08-03 10:18:47 -0500

    Fixed the tooltip

--- src/App.tsx
+++ src/App.tsx
@@ -191,7 +191,7 @@ const BuildingMaintenanceHeatMap: React.FC = () => {
                                                 >
                                                     {value > 0 ? <span className={white ? "text-white" : ""}>{value}</span>: ''}
                                                     {data && (
-                                                        <div className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 px-2 py-1 bg-gray-800 text-white text-xs rounded opacity-0 group-hover:opacity-100 transition-opacity z-10 whitespace-nowrap">
+                                                        <div className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-1 px-2 py-1 bg-gray-800 text-white text-xs rounded opacity-0 group-hover:opacity-100 transition-opacity z-10 whitespace-nowrap pointer-events-none">
                                                             Floor {floor}, Room {room}<br/>
                                                             Leaks: {data.leaks}<br/>
                                                             Drywall: {data.drywall}
